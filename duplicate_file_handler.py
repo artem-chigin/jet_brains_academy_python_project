3a54dd7274fa5dc591423ec94df82879d7ae913f
artem-chigin <chiartftc@gmail.com>
2022-07-11 14:16:07 +0400

    Tic-Tac-Toe with AI stage1 complete.

--- duplicate_file_handler.py
+++ duplicate_file_handler.py
@@ -12,18 +12,14 @@ msg_duplicates = ("Check for duplicates?", "Wrong option")
 msg_delete_files = ("Delete files?", "Wrong option")
 
 
-def calculate_hash(path_to_file_value):
-    """Calculate and return hash value"""
-
+def return_calculated_hash(path_to_file_value):
     with open(path_to_file_value, "rb") as checked_file:
         hash_val = hashlib.md5()
         hash_val.update(checked_file.read())
         return hash_val.hexdigest()
 
 
-def choose_from_two(value1, value2, msg):
-    """Choose between two pre-determined options"""
-
+def answer_is(value1, value2, msg):
     msg_intro, msg_outro = msg
     print(msg_intro)
     answer = str(input())
@@ -33,17 +29,15 @@ def choose_from_two(value1, value2, msg):
         return False
     else:
         print(msg_outro)
-        return choose_from_two(value1, value2, msg)
+        return answer_is(value1, value2, msg)
 
 
-def input_for_delete():
-    """Validate input for a correct format"""
-
+def get_file_ids_to_delete():
     print("Enter file numbers to delete:")
     numbers_of_files = str(input()).split()
     if any(map(lambda x: not x.isdigit(), numbers_of_files)) or not numbers_of_files:
         print("Wrong format")
-        return input_for_delete()
+        return get_file_ids_to_delete()
     else:
         return numbers_of_files
 
@@ -71,7 +65,7 @@ else:
 
     same_size_files = {s: p for s, p in path_and_size.items() if len(p) > 1}
 
-    direction_of_sorted = choose_from_two("1", "2", msg_sort_direction)
+    direction_of_sorted = answer_is("1", "2", msg_sort_direction)
 
     for byte in sorted(same_size_files.keys(), reverse=direction_of_sorted):
         print(str(byte) + " bytes")
@@ -81,11 +75,11 @@ else:
 
     files_with_hash_value = {}
 
-    if choose_from_two("yes", "no", msg_duplicates):
+    if answer_is("yes", "no", msg_duplicates):
         for _ in sorted(same_size_files.keys(), reverse=direction_of_sorted):
             files_with_hash_value[_] = files_with_hash_value.get(_, {})
             for file in same_size_files[_]:
-                hash_value = calculate_hash(file)
+                hash_value = return_calculated_hash(file)
                 files_with_hash_value[_][hash_value] = files_with_hash_value[_].get(hash_value, []) + [file]
 
     duplicates = {size: {h: p for h, p in hash_and_path.items() if len(p) > 1} for size, hash_and_path in files_with_hash_value.items()}
@@ -101,9 +95,9 @@ else:
                 count += 1
         print()
 
-    if choose_from_two("yes", "no", msg_delete_files):
+    if answer_is("yes", "no", msg_delete_files):
 
-        files_to_delete = set(input_for_delete())
+        files_to_delete = set(get_file_ids_to_delete())
 
         deleted_files_size = 0
 
